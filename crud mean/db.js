4cc96b735edeb87b62042e2f078f062bf45bda7f
wassim gtari <44499771+WassimGtari@users.noreply.github.com>
2019-03-24 16:45:02 +0100

    first commit

--- crud mean/db.js
+++
@@ -1,8 +0,0 @@
- const mongoose = require('mongoose');
- mongoose.connect('mongodb://localhost:27017/crudmean', (err)=>{
-     if(!err)
-     console.log("mongodb connection succeeded...");
-     else
-     console.log("erreur in db connection "+ JSON.stringify(err,undefined,2));
- });
- module.exports=mongoose;
--- crud mean/model/employee.js
+++
@@ -1,9 +0,0 @@
-const mongosse = require('mongoose');
-var employee= mongosse.model('employee',{
-name: {type :String },
-position: {type:String},
-office: {type:String},
-salaire: {type:Number}
-
-});
-module.exports={ employee } ;
